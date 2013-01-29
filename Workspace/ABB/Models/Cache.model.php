47d0ed1cf7480897a647209eb2f2756f1c060744
Heine Furubotten <hfurubotten@gmail.com>
2013-01-29 18:23:18 +0100

    Lagt til metoder i cache modelen

--- Workspace/ABB/Models/Cache.model.php
+++ Workspace/ABB/Models/Cache.model.php
@@ -2,6 +2,34 @@
 
 class Cache {
 	
+	public $extensionInstalled = false;
+	private $lockname = "_lock";
+	
+	public function __construct(){
+		$this->$extensionInstalled = extension_loaded("apc");
+	}
+	
+	public function lock(){
+		while(!apc_add($this->lockname)){
+			usleep(1000);
+		}
+	}
+	
+	public function apc_unlock(){
+		apc_delete($this->lockname);
+	}
+	
+	public function getCacheData(){
+		
+	}
+	
+	public function setCacheData($values){
+		
+	}
+	
+	public function removeCacheData(){
+		
+	}
 	
 }
 
