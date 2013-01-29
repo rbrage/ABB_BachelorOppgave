bde6205b6cc0c946ac615babbc70cc8b3b256771
Heine Furubotten <hfurubotten@gmail.com>
2013-01-29 22:01:54 +0100

    Modelklassen vil gi exception hvis ikke apc er installert

--- Workspace/ABB/Models/Cache.model.php
+++ Workspace/ABB/Models/Cache.model.php
@@ -9,25 +9,30 @@ class Cache {
 		$this->$extensionInstalled = extension_loaded("apc");
 	}
 	
-	public function lock(){
+	public function apc_lock(){
+		if(!$this->extensionInstalled) throw new Exception("The Extension APC is not installed. Install APC before using this modelclass.");
 		while(!apc_add($this->lockname)){
 			usleep(1000);
 		}
 	}
 	
 	public function apc_unlock(){
+		if(!$this->extensionInstalled) throw new Exception("The Extension APC is not installed. Install APC before using this modelclass.");
 		apc_delete($this->lockname);
 	}
 	
 	public function getCacheData(){
+		if(!$this->extensionInstalled) throw new Exception("The Extension APC is not installed. Install APC before using this modelclass.");
 		
 	}
 	
 	public function setCacheData($values){
+		if(!$this->extensionInstalled) throw new Exception("The Extension APC is not installed. Install APC before using this modelclass.");
 		
 	}
 	
 	public function removeCacheData(){
+		if(!$this->extensionInstalled) throw new Exception("The Extension APC is not installed. Install APC before using this modelclass.");
 		
 	}
 	
