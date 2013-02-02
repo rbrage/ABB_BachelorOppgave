9ff839f2cfab4cd125c04d3ab0b5bb6cbffa4ab8
Heine Furubotten <hfurubotten@gmail.com>
2013-02-02 23:21:16 +0100

    Lagt med tidspunkter i debugeren

--- Workspace/ABB/mvcclasses/Debuger.php
+++ Workspace/ABB/mvcclasses/Debuger.php
@@ -20,7 +20,7 @@ class Debuger {
 		}
 
 		if((self::$SendDebuginfoToBrowser && $id == NULL) || (self::$DebugIDinfo[$id][self::BrowserArrayNum]))
-			echo "Debugerpoint " . self::$counter . ": "  . $msg . "\n";
+			echo "Debugerpoint " . self::$counter . ", at " . microtime() . ": "  . $msg . "\n";
 
 		self::$counter++;
 	}
