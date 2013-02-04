69956560c25a40a37ec4d6eba09eebdb3228e539
Heine Furubotten <hfurubotten@gmail.com>
2013-02-04 20:42:07 +0100

    implementerte en finere vei til å legge til ting i viewmodelen.
    Dette vil ikke føre til notice eller warning selv om alt er kodet riktig

--- Workspace/ABB/mvcclasses/ViewModel.php
+++ Workspace/ABB/mvcclasses/ViewModel.php
@@ -1,9 +1,27 @@
 <?php
 
 class ViewModel {
+	
+	private $data = array();
 
 	public function Add($name, $value){
 		$this->$name = $value;
+	}
+	
+	public function __get($name){
+		return $this->data[$name];
+	}
+	
+	public function __set($name, $value){
+		$this->data[$name] = $value;
+	}
+	
+	public function __isset($name){
+		return isset($this->data[$name]);
+	}
+	
+	public function __unset($name){
+		unset($this->data[$name]);
 	}
 
 }
