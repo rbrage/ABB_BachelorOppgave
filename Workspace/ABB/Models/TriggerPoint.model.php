b99df6ec4213f76035dd7389bfe4fabf7b7816d4
Heine Furubotten <hfurubotten@gmail.com>
2013-02-04 20:49:16 +0100

    Lagt til metoder for å kunne behandle triggerpointet.

--- Workspace/ABB/Models/TriggerPoint.model.php
+++ Workspace/ABB/Models/TriggerPoint.model.php
@@ -2,11 +2,60 @@
 
 class TriggerPoint {
 	
-	public $x = -1;
-	public $y = -1; 
-	public $z = -1;
+	public $x;
+	public $y; 
+	public $z;
 	
-	public $timestamp = -1;
+	public $timestamp;
 	public $img;
+	
+	public function __construct($x = -1, $y = -1, $z = -1, $timestamp = -1, $img = null){
+		$this->x = $x;
+		$this->y = $y;
+		$this->z = $z;
+		
+		$this->timestamp = $timestamp;
+		$this->img = $img;
+	}
+	
+	public function getX(){
+		return $this->x;
+	}
+	
+	public function getY(){
+		return $this->y;
+	}
+	
+	public function getZ(){
+		return $this->z;
+	}
+	
+	public function getTimestemp(){
+		return $this->timestamp;
+	}
+	
+	public function getImg(){
+		return $this->img;
+	}
+	
+	public function setX($x){
+		$this->x = $x;
+	}
+	
+	public function setY($y){
+		$this->y = $y;
+	}
+	
+	public function setZ($z){
+		$this->z = $z;
+	}
+	
+	public function setTimestemp($timestamp){
+		$this->timestamp = $timestamp;
+	}
+	
+	public function setImg($img){
+		$this->img = $img;
+	}
 	
 }
