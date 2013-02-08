c8076072dd2e0517046b5c2fea533b93dc6d4d99
Heine Furubotten <hfurubotten@gmail.com>
2013-02-08 02:27:27 +0100

    Implementert trådkjøring og prosessbar.
    Simulatoren kjører nå med nettverkskomunikasjon i egen tråd slik at GUI
    kan oppdateres mens den kjører, og det er lagt til en processbar slik at
    det er mulig å holde følge med hvor langt den har kommet i kjøringen.

--- Analyzing_simulation_program/src/Analyzing_simulation_program/Simulation_program.java
+++ Analyzing_simulation_program/src/Analyzing_simulation_program/Simulation_program.java
@@ -1,6 +1,7 @@
 package Analyzing_simulation_program;
 
 import java.awt.Container;
+import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.DataInputStream;
@@ -15,45 +16,51 @@ import java.util.Random;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JProgressBar;
 import javax.swing.JTextField;
 
 
 public class Simulation_program extends JFrame implements ActionListener
 {
-	  JPanel pane = new JPanel();
-	  JTextField textField;
-	  String [] args;
-	  Simulation_program(String [] args) // the frame constructor method
-	  {
-		  super("Simulation program"); setBounds(100,100,300,100);
-	    this.args = args;
-	    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	    Container con = this.getContentPane(); // inherit main frame
-	    con.add(pane); // add the panel to frame
-	    textField = new JTextField(20);
-	    textField.setText("10");
-	    pane.add(textField);
-	    JButton buttom = new JButton("Start");
-	    buttom.addActionListener(this);
-	    pane.add(buttom);
-	    setVisible(true); // display this frame
-	  }
-	  public void actionPerformed(ActionEvent evt) {
-	        String text = textField.getText();
-	        numberOfTrigerpoints = Integer.parseInt(text);
-	        startConection(numberOfTrigerpoints);
-	        OpenBrowser openBrowser = new OpenBrowser();
-	        URI uri = null;
-			try {
-				uri = new URI("http://127.0.0.1:8888");
-			} catch (URISyntaxException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-	        openBrowser.openWebpage(uri);
-	        
-	  }
-	  
+	JPanel pane = new JPanel();
+	JTextField textField;
+	JProgressBar bar;
+	String [] args;
+	boolean running = false;
+	Simulation_program(String [] args) // the frame constructor method
+	{
+		super("Simulation program"); setBounds(100,100,300,200);
+		this.args = args;
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		Container con = this.getContentPane(); // inherit main frame
+		con.add(pane); // add the panel to frame
+		textField = new JTextField(20);
+		textField.setText("10");
+		pane.add(textField);
+
+		bar = new JProgressBar();
+		bar.setPreferredSize(new Dimension(225, 15));
+		bar.setStringPainted(true);
+		pane.add(bar);
+
+		JButton buttom = new JButton("Start");
+		buttom.addActionListener(this);
+		pane.add(buttom);
+
+		setVisible(true); // display this frame
+	}
+	public void actionPerformed(ActionEvent evt) {
+		if(!running){
+			running = true;
+			String text = textField.getText();
+			numberOfTrigerpoints = Integer.parseInt(text);
+			Thread t = new Thread(new StartConection());
+			t.start();
+		}
+
+	}
+
 	private static int numberOfTrigerpoints;
 	static int x;
 	static int y; 
@@ -62,38 +69,41 @@ public class Simulation_program extends JFrame implements ActionListener
 
 	public static void main(String[] args) throws Exception {
 		new Simulation_program(args);
-		
+
 	}
-	
-	public void startConection(int numberOfTrigerpoints){
-		try{
-			String path = "http://127.0.0.1:8888/register/trigger/xml?";
-			URL url;
-			URLConnection urlConnection = null;
-			DataInputStream inStream;
 
-			int i=0;
-			while(i<numberOfTrigerpoints){
-				setNewTriggerpoint();
-				url = new URL(path+"x="+x+"&y="+y+"&z="+z+"&time="+timestamp);
-				urlConnection = url.openConnection();
-				((HttpURLConnection)urlConnection).setRequestMethod("POST");
-				urlConnection.setDoOutput(true);
-				System.out.println(url.getQuery());
-
-				inStream = new DataInputStream(urlConnection.getInputStream());
-
-				String buffer;
-				while((buffer = inStream.readLine()) != null) {
-					if(buffer.contains("false")){
-						System.out.println(buffer);
-					}
-				}
-				inStream.close();
-				i++;
-			}
-		}catch (Exception e) {}
-	}
+	//	public void startConection(int numberOfTrigerpoints){
+	//		try{
+	//			String path = "http://127.0.0.1:8888/register/trigger/xml?";
+	//			URL url;
+	//			URLConnection urlConnection = null;
+	//			DataInputStream inStream;
+	//
+	//			bar.setMinimum(0);
+	//			bar.setMaximum(numberOfTrigerpoints);
+	//			int i=0;
+	//			while(i<numberOfTrigerpoints){
+	//				setNewTriggerpoint();
+	//				url = new URL(path+"x="+x+"&y="+y+"&z="+z+"&time="+timestamp);
+	//				urlConnection = url.openConnection();
+	//				((HttpURLConnection)urlConnection).setRequestMethod("POST");
+	//				urlConnection.setDoOutput(true);
+	//				System.out.println(url.getQuery());
+	//
+	//				inStream = new DataInputStream(urlConnection.getInputStream());
+	//
+	//				String buffer;
+	//				while((buffer = inStream.readLine()) != null) {
+	//					if(buffer.contains("false")){
+	//						System.out.println(buffer);
+	//					}
+	//				}
+	//				inStream.close();
+	//				bar.setValue(i);
+	//				i++;
+	//			}
+	//		}catch (Exception e) {}
+	//	}
 
 	public static void setNewTriggerpoint(){
 		x = RandomNumber();
@@ -111,7 +121,56 @@ public class Simulation_program extends JFrame implements ActionListener
 		Calendar cal = Calendar.getInstance();
 		return cal.getTimeInMillis();
 	}
-	
+
+	private class StartConection implements Runnable {
+
+		@Override
+		public void run() {
+			try{
+				String path = "http://127.0.0.1:8888/register/trigger/xml?";
+				URL url;
+				URLConnection urlConnection = null;
+				DataInputStream inStream;
+
+				bar.setMinimum(0);
+				bar.setMaximum(numberOfTrigerpoints);
+				int i=0;
+				while(i<numberOfTrigerpoints){
+					setNewTriggerpoint();
+					url = new URL(path+"x="+x+"&y="+y+"&z="+z+"&time="+timestamp);
+					urlConnection = url.openConnection();
+					((HttpURLConnection)urlConnection).setRequestMethod("POST");
+					urlConnection.setDoOutput(true);
+					System.out.println(url.getQuery());
+
+					inStream = new DataInputStream(urlConnection.getInputStream());
+
+					String buffer;
+					while((buffer = inStream.readLine()) != null) {
+						if(buffer.contains("false")){
+							System.out.println(buffer);
+						}
+					}
+					inStream.close();
+					i++;
+					bar.setValue(i);
+				}
+			}catch (Exception e) {}
+			
+			OpenBrowser openBrowser = new OpenBrowser();
+			URI uri = null;
+			try {
+				uri = new URI("http://127.0.0.1:8888");
+			} catch (URISyntaxException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			openBrowser.openWebpage(uri);
+			running = false;
+		}
+
+	}
+
 }
 
 
