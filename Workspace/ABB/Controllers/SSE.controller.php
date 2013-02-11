ac9009fbe73c56730ff82108fc388061543b7771
Heine Furubotten <hfurubotten@gmail.com>
2013-02-11 02:37:47 +0100

    Lagt til live update av basic informasjon.

--- Workspace/ABB/Controllers/SSE.controller.php
+++ Workspace/ABB/Controllers/SSE.controller.php
@@ -3,38 +3,50 @@
 require_once("Models/CachedArrayList.php");
 
 class SSE extends Controller {
-
-	public function CachedListSize(){
-		$this->list = new CachedArrayList();
-
-		$this->viewmodel->listsize = $this->list->size();
-
+	
+	public function __construct($action, $urlvalues){
+		parent::__construct($action, $urlvalues);
+		@set_time_limit(0);
+		
 		header('Content-Type: text/event-stream');
 		header('Cache-Control: no-cache');
-		ob_flush();
-		flush();
+		ob_implicit_flush();
+		
+	}
 
-		$old = -1;
+	public function BasicInfo(){
+		$this->list = new CachedArrayList();
+		
+		$info = apc_cache_info("user", true);
+		$this->viewmodel->listmemory = $info["mem_size"]/1000 . "k";
+		
+		$oldsize = -1;
+		$oldmemory = -1;
 		while (true){
 			$size = $this->list->size();
-			if($old != $size){
-				$old = $size;
+			if($oldsize != $size){
+				$oldsize = $size;
+				echo "event: cachesize" . PHP_EOL;
 				echo "data: " . $size . PHP_EOL;
+				echo PHP_EOL;
 				ob_flush();
 				flush();
 			}
+			
+			$info = apc_cache_info("user", true);
+			$memory = $info["mem_size"]/1000;
+			
+			if($memory != $oldmemory){
+				$oldmemory = $memory;
+				echo "event: memorysize" . PHP_EOL;
+				echo "data: " . $memory . PHP_EOL;
+				echo PHP_EOL;
+				ob_flush();
+				flush();
+			}
+			
 			sleep(1);
 		}
-
-		//$this->View();
-	}
-
-	protected function View(){
-
-		header('Content-Type: text/event-stream');
-		header('Cache-Control: no-cache');
-
-		parent::View();
 	}
 }
 
