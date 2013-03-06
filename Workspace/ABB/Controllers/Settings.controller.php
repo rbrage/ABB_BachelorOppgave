562900682c15b7a08c8d0fa87c5d8c012f63426d
Heine Furubotten <hfurubotten@gmail.com>
2013-03-07 00:21:28 +0100

    Mulighet for opplasting av kløsteregenskaper

--- Workspace/ABB/Controllers/Settings.controller.php
+++ Workspace/ABB/Controllers/Settings.controller.php
@@ -1,11 +1,30 @@
 <?php
 
+require_once("Models/CachedSettings.php");
+
 class Settings extends Controller {
 	
+	public $settings;
+	
 	public function Index(){
+		$this->settings = new CachedSettings();
+		$this->viewmodel->settings = $this->settings;
 		$this->View();
 	}
 	
+	public function Cluster(){
+		$maxPoints = $this->urlvalues[CachedSettings::MAXPOINTSINCLUSTERANALYSIS];
+		$numClusters = $this->urlvalues[CachedSettings::NUMBEROFCLUSTERS];
+		$this->settings = new CachedSettings();
+		
+		if(is_numeric($maxPoints))
+			$this->settings->setSetting(CachedSettings::MAXPOINTSINCLUSTERANALYSIS, $maxPoints);
+			
+		if(is_numeric($numClusters))
+			$this->settings->setSetting(CachedSettings::NUMBEROFCLUSTERS, $numClusters);
+			
+		return $this->RedirectTo("./");
+	}
 }
 
 ?>
