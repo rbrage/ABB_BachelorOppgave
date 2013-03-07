aa6ea0fb499c47867cf558a175099210332cffc3
Heine Furubotten <hfurubotten@gmail.com>
2013-03-07 01:21:15 +0100

    Standard innstillinger lagt til.

--- Workspace/ABB/Models/CachedSettings.php
+++ Workspace/ABB/Models/CachedSettings.php
@@ -5,16 +5,32 @@ require_once("Models/Cache.model.php");
 class CachedSettings {
 	
 	const SETTINGSPREFIX = "Settings_";
+	const SETTINGSLOADEDTOCACHE = "LoadedToCache";
 	const NUMBEROFCLUSTERS = "Clusters";
 	const MAXPOINTSINCLUSTERANALYSIS = "MaxClusterPointsInAnalysis";
 	const KODETOMASTERPOINTTRIGGERING = "MasterpointTriggeringkode";
 	const KODETOTRIGGERPROGRAMSTART = "TriggerProgramStartkode";
 	const ANALYSECLUSTERSWHILESUBMITION = "";
 	
-	public $cache;
+	private $defaltvalues = array(
+			self::NUMBEROFCLUSTERS => 1,
+			self::MAXPOINTSINCLUSTERANALYSIS => 100,
+			self::KODETOMASTERPOINTTRIGGERING => "",
+			self::KODETOTRIGGERPROGRAMSTART => "",
+			self::ANALYSECLUSTERSWHILESUBMITION => false
+			);
+	
+	private $cache;
 	
 	public function __construct(){
 		$this->cache = new Cache();
+		if (!$this->cache->getCacheData(self::SETTINGSPREFIX . self::SETTINGSLOADEDTOCACHE)){
+			foreach ($this->defaltvalues as $key => $value){
+				$this->cache->setCacheData(self::SETTINGSPREFIX . $key, $value);
+			}
+			
+			$this->cache->setCacheData(self::SETTINGSPREFIX . self::SETTINGSLOADEDTOCACHE, true);
+		}
 	}
 	
 	public function getSetting($name){
@@ -25,7 +41,9 @@ class CachedSettings {
 		return $this->cache->setCacheData(self::SETTINGSPREFIX . $name, $value);
 	}
 	
-	
+	public function getDefaultSettings(){
+		return $this->defaltvalues;
+	}
 	
 }
 
