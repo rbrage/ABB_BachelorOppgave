1aa9adc9765e9d371e77891e8b77ac4cb092610b
Heine Furubotten <hfurubotten@gmail.com>
2013-03-15 17:47:37 +0100

    Legger phpscripts i riktig mappe automatisk.

--- serversetup.sh
+++ serversetup.sh
@@ -3,4 +3,5 @@ sudo apt-get install apache2
 sudo a2enmod rewrite
 sudo apt-get install php5 libapache2-mod-php5
 sudo apt-get install php-apc
+sudo yes | cp ./Workspace/ABB/* /var/www
 sudo service apache2 restart
