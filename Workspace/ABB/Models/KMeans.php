b11315ec31599dc64fa7a180acfd44ab6d4165c0
Heine Furubotten <hfurubotten@gmail.com>
2013-03-21 00:34:45 +0100

    Optimalisert kløsteralgoritme

--- Workspace/ABB/Models/KMeans.php
+++ Workspace/ABB/Models/KMeans.php
@@ -11,15 +11,19 @@ class KMeans {
 	private $cache;
 	private $clusterlist;
 	private $pointlist;
+	private $pointMoved;
 	
 	const CLUSTERLISTNAME = "CLUSTERANALYSIS";
 	const CLUSTERANALYSISRUNNINGNAME = "CLUSTERANALYSISRUNNING";
-	const CLUSTERTOUCHEDNAME = "Touched";
+// 	const CLUSTERTOUCHEDNAME = "Touched";
 	const CLUSTERCOUNTNAME = "CLUSTERCOUNT";
 // 	const CLUSTERARRAYNAME = "Clusterarray";
 // 	const CLUSTERHASINITIALCLUSTER = "ClusterHasInitialClusters";
 	
-	
+	/**
+	 * Creates a new instance of the KMeans class.
+	 * @param int $k
+	 */
 	public function __construct($k){
 		Debuger::SetupNewDebugID("KMeans");
 		Debuger::SetSendInfoToBrowser("KMeans", false);
@@ -29,6 +33,9 @@ class KMeans {
 		$this->pointlist = new CachedArrayList();
 	}
 	
+	/**
+	 * Starts the calculation of the clusters.
+	 */
 	public function calculateClusters(){
 		if(!$this->cache->hasKey(self::CLUSTERANALYSISRUNNINGNAME)){
 			$this->cache->setCacheData(self::CLUSTERANALYSISRUNNINGNAME, true);
@@ -40,24 +47,30 @@ class KMeans {
 				$this->cache->removeCacheData(self::CLUSTERANALYSISRUNNINGNAME);
 				return;
 			}
+
+			$this->updateClusterCenter();
 			
 			while(true){
 
-				$this->updateClusterCenter();
 				$this->reasignPointsToClusters();
 					
-				$tuched = false;
-				foreach($this->clusterlist->iterator() as $cluster){
-					if($cluster->getAdditionalInfo(self::CLUSTERTOUCHEDNAME)){
-						$tuched = true;
-						break;
-					}
-				}
+// 				$tuched = false;
+// 				foreach($this->clusterlist->iterator() as $cluster){
+// 					if($cluster->getAdditionalInfo(self::CLUSTERTOUCHEDNAME)){
+// 						$tuched = true;
+// 						break;
+// 					}
+// 				}
 					
-				if(!$tuched)
+// 				if(!$tuched)
+// 					break;
+				
+				if(!$this->pointMoved)
 					break;
 
+				$this->updateClusterCenter();
 			}
+
 			
 			$this->cache->removeCacheData(self::CLUSTERANALYSISRUNNINGNAME);
 		}
@@ -66,10 +79,17 @@ class KMeans {
 		}
 	}
 	
+	/**
+	 * 
+	 * @param int $numberOfPoints
+	 */
 	public function setNumberOfPointsToDeterminClusters($numberOfPoints){
 		$this->pointsToCrush = $numberOfPoints;
 	}
 	
+	/**
+	 * 
+	 */
 	private function asignInitialCluster(){
 		for($i = $this->clusterlist->size(); $i < $this->k && $i < $this->pointlist->size(); $i++){
 			Debuger::RegisterPoint("Asigning cluster " . $i, "KMeans");
@@ -77,38 +97,49 @@ class KMeans {
 			$point->setAsignedCluster($i);
 			$this->pointlist->set($i, $point, true);
 			
-			$point->addAdditionalInfo(self::CLUSTERTOUCHEDNAME, false);
+// 			$point->addAdditionalInfo(self::CLUSTERTOUCHEDNAME, false);
 			$point->addAdditionalInfo(self::CLUSTERCOUNTNAME, 0);
 // 			$point->addAdditionalInfo(self::CLUSTERARRAYNAME, array());
 			$this->clusterlist->set($i, $point);
 		}
 	}
 	
+	/**
+	 * 
+	 * @param int $pointNumber
+	 * @param int $clusterNumber
+	 */
 	private function putPointInCluster($pointNumber, $clusterNumber){
+		
+		$this->pointMoved = true;
+		
 		$point = $this->pointlist->get($pointNumber, true);
 		$oldclusternumber = $point->getAsignedCluster();
 		$point->setAsignedCluster($clusterNumber);
 		$this->pointlist->set($pointNumber, $point, true);
 		
 		// adding point to new cluster
-		$clusterpoint = $this->clusterlist->get($clusterNumber);
-		if($pointNumber < $this->pointsToCrush)
-			$clusterpoint->addAdditionalInfo(self::CLUSTERTOUCHEDNAME, true);
+// 		$clusterpoint = $this->clusterlist->get($clusterNumber, true);
+// 		if($pointNumber < $this->pointsToCrush)
+// 			$clusterpoint->addAdditionalInfo(self::CLUSTERTOUCHEDNAME, true);
 // 		$clusterarray = $clusterpoint->getAdditionalInfo(self::CLUSTERARRAYNAME);
 // 		$clusterarray[$pointNumber] = $pointNumber;
 // 		$clusterpoint->addAdditionalInfo(self::CLUSTERARRAYNAME, $clusterarray);
-		$this->clusterlist->set($clusterNumber, $clusterpoint);
+// 		$this->clusterlist->set($clusterNumber, $clusterpoint, true);
 		
 		// removing point from old cluster
-		$clusterpoint = $this->clusterlist->get($oldclusternumber);
-		if($pointNumber < $this->pointsToCrush)
-			$clusterpoint->addAdditionalInfo(self::CLUSTERTOUCHEDNAME, true);
+// 		$clusterpoint = $this->clusterlist->get($oldclusternumber);
+// 		if($pointNumber < $this->pointsToCrush)
+// 			$clusterpoint->addAdditionalInfo(self::CLUSTERTOUCHEDNAME, true);
 // 		$clusterarray = $clusterpoint->getAdditionalInfo(self::CLUSTERARRAYNAME);
 // 		$clusterarray[$pointNumber] = -1;
 // 		$clusterpoint->addAdditionalInfo(self::CLUSTERARRAYNAME, $clusterarray);
-		$this->clusterlist->set($oldclusternumber, $clusterpoint);
+// 		$this->clusterlist->set($oldclusternumber, $clusterpoint, true);
 	}
 	
+	/**
+	 * 
+	 */
 	private function reasignPointsToClusters(){
 		for($i = 0; $i < $this->pointsToCrush && $i < $this->pointlist->size(); $i++){
 			Debuger::RegisterPoint("Trying to reasigning point " . $i, "KMeans");
@@ -133,13 +164,16 @@ class KMeans {
 		}
 	}
 	
+	/**
+	 * 
+	 */
 	public function asignAllPointsToClusters(){
 		foreach ($this->pointlist->iterator() as $i => $point){
 			Debuger::RegisterPoint("Trying to reasigning point " . $i, "KMeans");
 			$shortestcluster = 0;
 			$shortestdistance = -1;
-			foreach ($this->clusterlist as $c => $cluster){
-				$distance = $this->distance($point, cluster);
+			foreach ($this->clusterlist->iterator() as $c => $cluster){
+				$distance = $this->distance($point, $cluster);
 				Debuger::RegisterPoint("Distance to cluster " . $c . ": " . $distance, "KMeans");
 				if($distance < $shortestdistance || $shortestdistance < 0){
 					$shortestcluster = $c;
@@ -156,6 +190,9 @@ class KMeans {
 		}
 	}
 	
+	/**
+	 * 
+	 */
 	private function updateClusterCenter(){
 		$sumarray = array();
 		$countarray = array();
@@ -187,12 +224,16 @@ class KMeans {
 				$clusterpoint->z = $sum["z"]/$countarray[$cluster];
 				
 				$clusterpoint->addAdditionalInfo(self::CLUSTERCOUNTNAME, $countarray[$cluster]);
-				$clusterpoint->addAdditionalInfo(self::CLUSTERTOUCHEDNAME, false);
+// 				$clusterpoint->addAdditionalInfo(self::CLUSTERTOUCHEDNAME, false);
+
+				$this->pointMoved = false;
 			}
 			else{
 				$clusterpoint = $this->pointlist->get($this->pointlist->size() - 1);
 				$clusterpoint->addAdditionalInfo(self::CLUSTERCOUNTNAME, 0);
-				$clusterpoint->addAdditionalInfo(self::CLUSTERTOUCHEDNAME, true);
+// 				$clusterpoint->addAdditionalInfo(self::CLUSTERTOUCHEDNAME, true);
+				
+				$this->pointMoved = true;
 			}
 			
 			$this->clusterlist->set($cluster, $clusterpoint, true);
@@ -201,15 +242,34 @@ class KMeans {
 		}
 	}
 	
+	/**
+	 * 
+	 * @param TriggerPoint $first
+	 * @param TriggerPoint $second
+	 * @return float
+	 */
 	private function distance($first, $second){
 		return sqrt(pow(($first->x - $second->x), 2) + pow(($first->y - $second->y), 2) + pow(($first->z - $second->z), 2));
 	}
 	
+	/**
+	 * 
+	 */
 	public function forceNewAnalysis(){
 		$this->clusterlist->clear();
 		$this->cache->removeCacheData(self::CLUSTERANALYSISRUNNINGNAME);
+		for($i = 0; $i < $this->pointsToCrush; $i++){
+			$point = $this->pointlist->get($i, true);
+			$point->cluster = 0;
+			$this->pointlist->set($i, $point, true);
+		}
 	}
 	
+	/**
+	 * 
+	 * @param TriggerPoint $point
+	 * @return boolean
+	 */
 	public function asignCluster(&$point){
 
 		if($this->k > $this->clusterlist->size()){
