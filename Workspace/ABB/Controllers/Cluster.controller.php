c81660cc6f43d679f5a6caa93411ba2d20228d93
Heine Furubotten <hfurubotten@gmail.com>
2013-03-24 01:52:31 +0100

    Skjekk hvilke notasjon skriptet skal svare i.

--- Workspace/ABB/Controllers/Cluster.controller.php
+++ Workspace/ABB/Controllers/Cluster.controller.php
@@ -11,6 +11,9 @@ class Cluster extends Controller {
 	private $settings;
 	private $cluster;
 
+	/**
+	 * Loads the main cluster analysis site. All user interaction should be done here. Answers in HTML5.
+	 */
 	public function index(){
 		$this->clusterlist = new CachedArrayList(KMeans::CLUSTERLISTNAME);
 		$this->settings = new CachedSettings();
@@ -20,14 +23,44 @@ class Cluster extends Controller {
 		return $this->View();
 	}
 
+	/**
+	 * Clears all clusters that has been calculated before. Answers either in json or xml, given in id that have to be set.
+	 * @param String $id
+	 */
 	public function reset($id){
+		$this->viewmodel->error = false;
+		$this->viewmodel->noCoding = false;
+		
+		if($id != "json" && $id != "xml"){
+			$this->viewmodel->error = true;
+			$this->viewmodel->errmsg = "The coding you requested is not recognized.";
+			$this->viewmodel->noCoding = true;
+			return $this->View();
+		}
+		$this->viewmodel->returnCoding = $id;
+		
 		$this->clusterlist = new CachedArrayList(KMeans::CLUSTERLISTNAME);
 		$this->clusterlist->clear();
 		
 		echo json_encode(array("msg" => "The clusterlist is cleared."));
 	}
 
+	/**
+	 * Runs a new cluster analysis. If a analysis if already run it will continue on that one. Answers either in json or xml, given in id that have to be set.
+	 * @param String $id
+	 */
 	public function run($id){
+		$this->viewmodel->error = false;
+		$this->viewmodel->noCoding = false;
+		
+		if($id != "json" && $id != "xml"){
+			$this->viewmodel->error = true;
+			$this->viewmodel->errmsg = "The coding you requested is not recognized.";
+			$this->viewmodel->noCoding = true;
+			return $this->View();
+		}
+		$this->viewmodel->returnCoding = $id;
+		
 		$this->settings = new CachedSettings();
 		$this->cluster = new KMeans($this->settings->getSetting(CachedSettings::NUMBEROFCLUSTERS));
 		$this->cluster->setNumberOfPointsToDeterminClusters($this->settings->getSetting(CachedSettings::MAXPOINTSINCLUSTERANALYSIS));
@@ -37,20 +70,69 @@ class Cluster extends Controller {
 		echo json_encode(array("msg" => "The clusters are done calculating."));
 	}
 	
+	/**
+	 * Forces a new cluster analysis by clearing all previus information first. Answers either in json or xml, given in id that have to be set. 
+	 * @param unknown_type $id
+	 */
 	public function force($id){
+		$this->viewmodel->error = false;
+		$this->viewmodel->noCoding = false;
+		
+		if($id != "json" && $id != "xml"){
+			$this->viewmodel->error = true;
+			$this->viewmodel->errmsg = "The coding you requested is not recognized.";
+			$this->viewmodel->noCoding = true;
+			return $this->View();
+		}
+		$this->viewmodel->returnCoding = $id;
+		
 		$this->settings = new CachedSettings();
 		$this->cluster = new KMeans($this->settings->getSetting(CachedSettings::NUMBEROFCLUSTERS));
 		$this->cluster->setNumberOfPointsToDeterminClusters($this->settings->getSetting(CachedSettings::MAXPOINTSINCLUSTERANALYSIS));
 		$this->cluster->forceNewAnalysis();
 		$this->cluster->calculateClusters();
+		$this->cluster->asignAllPointsToClusters();
 		echo json_encode(array("msg" => "The new analysis is done."));
 	}
 	
+	/**
+	 * Reasigns all points to the defined clusters. Answers either in json or xml, given in id that have to be set.
+	 * @param String $id
+	 */
 	public function reasign($id){
+		$this->viewmodel->error = false;
+		$this->viewmodel->noCoding = false;
+		
+		if($id != "json" && $id != "xml"){
+			$this->viewmodel->error = true;
+			$this->viewmodel->errmsg = "The coding you requested is not recognized.";
+			$this->viewmodel->noCoding = true;
+			return $this->View();
+		}
+		$this->viewmodel->returnCoding = $id;
+		
 		$this->settings = new CachedSettings();
 		$this->cluster = new KMeans($this->settings->getSetting(CachedSettings::NUMBEROFCLUSTERS));
 		$this->cluster->asignAllPointsToClusters();
 		echo json_encode(array("msg" => "All points are now in nearest cluster."));
+	}
+	
+	/**
+	 * Gives all or one of the points that defines cluster sentrums. Answers either in json or xml, given in id that have to be set.
+	 * @param String $id
+	 */
+	public function points($id){
+		$this->viewmodel->error = false;
+		$this->viewmodel->noCoding = false;
+		
+		if($id != "json" && $id != "xml"){
+			$this->viewmodel->error = true;
+			$this->viewmodel->errmsg = "The coding you requested is not recognized.";
+			$this->viewmodel->noCoding = true;
+			return $this->View();
+		}
+		$this->viewmodel->returnCoding = $id;
+		
 	}
 
 }
