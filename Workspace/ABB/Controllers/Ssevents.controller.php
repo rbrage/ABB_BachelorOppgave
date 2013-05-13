094687bf573dc4ee3f3a494d881c53f529bb6158
Heine Furubotten <hfurubotten@gmail.com>
2013-05-13 10:25:00 +0200

    filnavn endring del 2

---
+++ Workspace/ABB/Controllers/Ssevents.controller.php
@@ -0,0 +1,76 @@
+<?php
+
+require_once 'Models/Cache.php';
+require_once 'Models/CachedArrayList.php';
+require_once 'Models/SSE.php';
+require_once 'Models/ListNames.php';
+
+class SSEvents extends Controller {
+
+	private $pointlist;
+	private $clusterlist;
+	private $outlierlist;
+	private $masterlist;
+	private $sse;
+	private $cache;
+	
+	/**
+	 * Provides a SSE connection that gives basic info in the system.
+	 */
+	public function BasicInfo(){
+		$this->pointlist = new CachedArrayList();
+		$this->clusterlist = new CachedArrayList(ListNames::CLUSTERLISTNAME);
+		$this->masterlist = new CachedArrayList(ListNames::MASTERPOINTLISTNAME);
+		$this->outlierlist = new CachedArrayList(ListNames::OUTLYINGPOINTLISTNAME);
+		$this->sse = new SSE();
+		$this->cache = new Cache();
+		
+		$info = $this->cache->getCacheInfo();
+		
+		$this->sse->start();
+		
+		$oldpointsize = -1;
+		$oldmemory = -1;
+		$oldclustersize = -1;
+		$oldoutliersize = -1;
+		$oldmastersize = -1;
+		while (true){
+			$size = $this->pointlist->size();
+			if($oldpointsize != $size){
+				$oldpointsize = $size;
+				$this->sse->sendData("pointsize", $size);
+			}
+			
+			$size = $this->clusterlist->size();
+			if($oldclustersize != $size){
+				$oldclustersize = $size;
+				$this->sse->sendData("clustersize", $size);
+			}
+			
+			$size = $this->masterlist->size();
+			if($oldmastersize != $size){
+				$oldmastersize = $size;
+				$this->sse->sendData("mastersize", $size);
+			}
+			
+			$size = $this->outlierlist->size();
+			if($oldoutliersize != $size){
+				$oldoutliersize = $size;
+				$this->sse->sendData("outliersize", $size);
+			}
+			
+			$info = $this->cache->getCacheInfo();
+			$memory = round($info["mem_size"]/1000, 2);
+			
+			if($memory != $oldmemory){
+				$oldmemory = $memory;
+				$this->sse->sendData("usedmemory", $memory);
+			}
+			
+			// sover i 1 sec
+			sleep(1);
+		}
+	}
+}
+
+?>
